40f724dd33a184c175cb94ec30b626abac14e488
franzparks <franzparks@gmail.com>
2016-08-12 22:46:31 -0700

    update routes

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -16,16 +16,16 @@
         controllerAs: 'main'
       })
       // Pages
-    .when("/about", {templateUrl: "app/components/about/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "app/components/faq/faq.html", controller: "PageCtrl"})
-    .when("/pricing", {templateUrl: "app/components/pricing/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "app/components/services/services.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "app/components/contact/contact.html", controller: "PageCtrl"})
+    .when("/about", {templateUrl: "app/components/about/about.html", controller: "AboutCtrl"})
+    .when("/faq", {templateUrl: "app/components/faq/faq.html", controller: "FaqCtrl"})
+    .when("/pricing", {templateUrl: "app/components/pricing/pricing.html", controller: "PricingCtrl"})
+    .when("/services", {templateUrl: "app/components/services/services.html", controller: "ServicesCtrl"})
+    .when("/contact", {templateUrl: "app/components/contact/contact.html", controller: "ContactCtrl"})
     // Blog
     .when("/blog", {templateUrl: "app/components/blog/blog.html", controller: "BlogCtrl"})
     .when("/blog/post", {templateUrl: "app/components/blog/blog_item.html", controller: "BlogCtrl"})
     // else 404
-    .otherwise("/404", {templateUrl: "app/components/404/404.html", controller: "PageCtrl"});
+    .otherwise("/404", {templateUrl: "app/components/404/404.html", controller: "404Ctrl"});
   }
 
 })();
