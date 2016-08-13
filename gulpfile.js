72c7f5b5782deb289942f1a4a514ef50b7525183
franzparks <franzparks@gmail.com>
2016-08-13 12:36:29 -0700

    add sw files

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,11 @@
 
 var fs = require('fs');
 var gulp = require('gulp');
+/****************/
+var packageJson = require('./package.json');
+var path = require('path');
+var swPrecache = require('sw-precache');
+/****************/
 
 /**
  *  This will load all js or coffee files in the gulp directory
@@ -26,4 +31,37 @@ fs.readdirSync('./gulp').filter(function(file) {
  */
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
+});
+
+
+/***************/
+function generateServiceWorkerFileContents(rootDir, handleFetch, callback) {
+  var config = {
+    cacheId: packageJson.name,
+    handleFetch: handleFetch,
+    logger: console.log,
+    dynamicUrlToDependencies: {
+      './': [path.join(rootDir, 'views', 'index.ejs')],
+    },
+    staticFileGlobs: [
+      rootDir + '/stylesheets/**.css',
+      rootDir + '/views/**.html',
+      rootDir + '/images/**.*',
+      rootDir + '/bower_components/**/**.js',
+      rootDir + '/libs/.js',
+      rootDir + '/**.js'
+    ],
+    stripPrefix: path.join(rootDir, path.sep)
+  };
+
+  swPrecache(config, callback);
+}
+
+gulp.task('build', function(callback) {
+  generateServiceWorkerFileContents('./app', true, function(error, serviceWorkerFileContents) {
+    if (error) {
+      return callback(error);
+    }
+    fs.writeFile(path.join('service-worker.js'), serviceWorkerFileContents, callback);
+  });
 });
