cc8defc682d2bbf5eb5ee4a07df38bfa578336fa
franzparks <franzparks@gmail.com>
2016-08-12 22:40:39 -0700

    add blog controller

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -5,3 +5,14 @@
     .module('treadstone', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr']);
 
 })();
+
+
+// Activates the Carousel
+  //$('.carousel').carousel({
+  //  interval: 5000
+  //});
+
+  // Activates Tooltips for Social Links
+  //$('.tooltip-social').tooltip({
+  //  selector: "a[data-toggle=tooltip]"
+  //})
