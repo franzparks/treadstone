8953a3fc0d039e3e81f60c2245c2b1e71807cd43
franzparks <franzparks@gmail.com>
2016-08-14 14:41:16 -0700

    use  service data

--- src/app/components/about/about.controller.js
+++ src/app/components/about/about.controller.js
@@ -7,7 +7,13 @@
 
   /** @ngInject */
   function AboutController($log, aboutUsService) {
-    //svar vm = this;
+    var vm = this;
+
+    vm.team = [];
+    vm.customers = [];
+
+    vm.team = aboutUsService.getTeam();
+    vm.customers = aboutUsService.getCustomers();
 
     //$log.debug(' About controller called');
 }
