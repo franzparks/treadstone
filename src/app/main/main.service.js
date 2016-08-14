390cf63c235be5ac2d920842d1f9a644b3c52a4c
franzparks <franzparks@gmail.com>
2016-08-14 12:56:42 -0700

    add data to service

--- src/app/main/main.service.js
+++ src/app/main/main.service.js
@@ -7,24 +7,46 @@
 
   /** @ngInject */
   function mainService() {
+
     var featuredPortfolios = [
-      {
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
+      { 
+        'url': 'http://lorempixel.com/700/450/abstract/8/Carlos-The-Jackal'
       },
-      
+      {
+        'url' : 'http://lorempixel.com/700/450/fashion/6/Marie'
+      },
+      {
+        'url':'http://lorempixel.com/700/450/nature/4/TreadStone-Grounds'
+      },
+      {
+        'url':'http://lorempixel.com/700/450/nature/2/Tam-Quan'
+      },
+      {
+        'url':'http://lorempixel.com/700/450/people/3/Carlos-Old-Soldiers'
+      },
+      {
+        'url':'http://lorempixel.com/700/450/abstract/6/Wombosi'
+      }    
      
     ];
 
     var featuredArtists = [
       {
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
+        'author': 'Alexander Conklin',
+        'url': 'http://lorempixel.com/700/450/abstract/5/Alex-Conklin',
+        'description': 'One of the most wonderful paintings done during the days of treadstone\
+                      and on and on....\
+        ',
       },
+
+      {
+        'author': 'David Webb',
+        'url': 'http://lorempixel.com/700/450/abstract/1/Jason-Bourne-Medusa-Chameleon',
+        'description': 'One of the most wonderful paintings done during the days of treadstone\
+                      and on and on....\
+        ',
+      },
+      
       
      
     ];
