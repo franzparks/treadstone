bfd1b5982125e29e5eba1c464c76921a9c5deec6
franzparks <franzparks@gmail.com>
2016-08-14 14:07:27 -0700

    add data to pricing service

--- src/app/components/pricing/pricing.service.js
+++ src/app/components/pricing/pricing.service.js
@@ -9,18 +9,44 @@
   function pricingService() {
     var data = [
       {
-        'id' : 'collapseOne',
-        'question' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
-        'answer' : 
-        'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad \
-        squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa \
-        nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid \
-        single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft \
-        beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice\
-        lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you\
-        probably haven\'t heard of them accusamus labore sustainable VHS.'
+        'type' : 'Basic',
+        'price': '$19',
+        'price_cents':'99',
+        'price_month':'mo.'
+        'paintings' : '5 Paintings',
+        'storage':'5 GB of Storage For Images',
+        'transportation':'No Transportation',
+        'guarantee':'No Guarantee'
       },
-
+      {
+        'type' : 'Plus',
+        'price': '$39',
+        'price_cents':'99',
+        'price_month':'mo.'
+        'paintings' : '10 Paintings',
+        'storage':'10 GB of Storage For Images',
+        'transportation':'Free Transportation For Paintings',
+        'guarantee':'1 Year Guarantee'
+      },
+      {
+        'type' : 'Premium Best Value!',
+        'price': '$49',
+        'price_cents':'99',
+        'price_month':'mo.'
+        'paintings' : '10 Paintings',
+        'storage':'50 GB of Storage For Images',
+        'transportation':'Free Transportation For Paintings',
+        'guarantee':'2 Year Guarantee'
+      },{
+        'type' : 'Ultimate',
+        'price': '$99',
+        'price_cents':'99',
+        'price_month':'mo.'
+        'paintings' : '50 Paintings',
+        'storage':'150 GB of Storage',
+        'transportation':'Free Transportation For Paintings',
+        'guarantee':'Unlimited Warranty'
+      },
 
     ];
 
