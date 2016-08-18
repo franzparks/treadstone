a1bc6cb0efd9149f9f114c7b8421414e4d7063da
franzparks <franzparks@gmail.com>
2016-08-18 14:10:58 -0700

    add model data to controller

--- src/app/components/contact/contact.controller.js
+++ src/app/components/contact/contact.controller.js
@@ -7,9 +7,26 @@
 
   /** @ngInject */
   function ContactController($log) {
-    //var vm = this;
+    var vm = this;
 
-    $log.debug(' Contact controller called');
+    vm.form_submitted = false;
+
+    vm.name = '';
+
+    vm.email = '';
+
+    vm.phone = '';
+
+    vm.message = '';
+
+    vm.submit = function(message){
+      //$log.debug(' I was called!'+ Object.keys(user));
+      vm.form_submitted = true;
+
+    };
+
+
+    //$log.debug(' Contact controller called');
 }
 
 })();
