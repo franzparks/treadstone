19ae8374f8eb6cada13e7fa0cf900dd7be33d20f
franzparks <franzparks@gmail.com>
2016-08-19 22:51:46 -0700

    use https to get placeholder images

--- src/app/components/about/about.service.js
+++ src/app/components/about/about.service.js
@@ -12,37 +12,37 @@
         'name' : 'Robert Ludlum',
         'title' : 'C.E.O',
         'dept': 'The Company',
-        'url':'http://placehold.it/750x450'
+        'url':'https://placehold.it/750x450'
       },
       {
         'name' : 'Francis Phiri',
         'title' : 'C.T.O',
         'dept': 'Tech Gadgets Dept',
-        'url':'http://placehold.it/750x450'
+        'url':'https://placehold.it/750x450'
       },
       {
         'name' : 'Brandon Scofield',
         'title' : 'Artist',
         'dept': 'Art Dept',
-        'url':'http://placehold.it/750x450'
+        'url':'https://placehold.it/750x450'
       },
       {
         'name' : 'Amanda Waller',
         'title' : 'The VIP',
         'dept': 'S Squad',
-        'url':'http://placehold.it/750x450'
+        'url':'https://placehold.it/750x450'
       },
       {
         'name' : 'Jack Reacher',
         'title' : 'Fixer',
         'dept': 'Marketing',
-        'url':'http://placehold.it/750x450'
+        'url':'https://placehold.it/750x450'
       },
       {
         'name' : 'Vasili Taleniekov',
         'dept' : 'Clandestine Ops',
         'title':'FSB Liason',
-        'url':'http://placehold.it/750x450'
+        'url':'https://placehold.it/750x450'
 
       }
 
@@ -51,22 +51,22 @@
 
     var customers = [
       {
-        'url' : 'http://placehold.it/500x300'
+        'url' : 'https://placehold.it/500x300'
       },
       {
-        'url' : 'http://placehold.it/500x300'
+        'url' : 'https://placehold.it/500x300'
       },
       {
-        'url' : 'http://placehold.it/500x300'
+        'url' : 'https://placehold.it/500x300'
       },
       {
-        'url' : 'http://placehold.it/500x300'
+        'url' : 'https://placehold.it/500x300'
       },
       {
-        'url' : 'http://placehold.it/500x300'
+        'url' : 'https://placehold.it/500x300'
       },
       {
-        'url' : 'http://placehold.it/500x300'
+        'url' : 'https://placehold.it/500x300'
       }
 
     ];
