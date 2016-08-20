1f8e7fd9bdad9ebc5e360525d281a6ce17031f04
franzparks <franzparks@gmail.com>
2016-08-19 21:49:13 -0700

    clean up

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -5,14 +5,3 @@
     .module('treadstone', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr']);
 
 })();
-
-
-// Activates the Carousel
-  //$('.carousel').carousel({
-  //  interval: 5000
-  //});
-
-  // Activates Tooltips for Social Links
-  //$('.tooltip-social').tooltip({
-  //  selector: "a[data-toggle=tooltip]"
-  //})
